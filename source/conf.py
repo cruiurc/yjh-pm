aa81abd8e44156af4f4f275e1c21c39be835a1ab
cruiurc <crui@outlook.com>
2022-06-24 12:58:12 +0800

    requirements

--- source/conf.py
+++ source/conf.py
@@ -28,6 +28,7 @@ author = 'c'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'plantweb.directive',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
